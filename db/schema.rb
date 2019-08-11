cafaeae2e058a0fd9a31aa0e266008987198d3c2
BomjMan <moiseevnvr49@gmail.com>
2019-08-11 14:06:51 +0300

    changed db on postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_08_09_103735) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "etclassics", force: :cascade do |t|
     t.float "profit"
     t.datetime "created_at", null: false
